07ce4dee8a0a5e701ef0e4221551ab17fdc83868
teonateona12 <teona.piranishvili1@gmail.com>
2024-02-09 01:09:07 -0800

    feat: create slider for crypto

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <>
       <Header />
-      <div className=" mt-[50px] bigTablet:mt-[90px]"></div>
+      <div className=" mt-[50px] bigTablet:mt-[90px]">
+        <Home />
+      </div>
       <Footer />
     </>
   );
